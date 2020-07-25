a9c5868aceae4548f44a8ecc14ea2fef21f9cd6f
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-25 09:20:03 -0300

    Iniciando a aplicação

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// 1) Temos que referenciar o input
+let input = document.querySelector('input[name=tarefa]');
 
+// 2) Temos que referenciar o button
+let btn = document.querySelector('#botao');
+
+// 3) Temos que referenciar a lista
+let lista = document.querySelector('#lista');
+
+let tarefas = [
+    'Ver filmes',
+    'Ler livro',
+    'Estudar React',
+    'Aprender Node'
+]
+
+//<li class="list-group-item list-group-item-action">Estudar React</li>
